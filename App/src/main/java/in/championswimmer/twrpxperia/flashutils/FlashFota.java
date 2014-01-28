8c1127c91b4c43b42482ccdac132eeb37ab756db
Arnav Gupta <championswimmer@aokp.co>
2014-01-29 00:55:07 +0530

    feedback (success/failure) : format, backup, restore, flash

--- App/src/main/java/in/championswimmer/twrpxperia/flashutils/FlashFota.java
+++ App/src/main/java/in/championswimmer/twrpxperia/flashutils/FlashFota.java
@@ -3,9 +3,14 @@ package in.championswimmer.twrpxperia.flashutils;
 import android.content.Context;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import in.championswimmer.twrpxperia.R;
 
@@ -21,6 +26,7 @@ public class FlashFota {
     private String XPERIA = "yuga";
     private String method;
     SaveDir dir = new SaveDir();
+    private Context c;
 
     public Boolean hasRoot() {
         Process root;
@@ -46,6 +52,7 @@ public class FlashFota {
         String[] prop = context.getResources().getStringArray(R.array.supported_device_prop);
         String[] codename = context.getResources().getStringArray(R.array.supported_device_codename);
         String[] fotaPath = context.getResources().getStringArray(R.array.fota_path);
+        c = context;
 
         Log.d(LOG_TAG, "new object to manipulate fota created");
 
@@ -62,6 +69,8 @@ public class FlashFota {
         Log.d(LOG_TAG, "preparing to format fota");
         Process p;
         DataOutputStream os;
+        InputStream is;
+        String res = "";
         //the format command
         String[] cmds = new String[]{
                 "dd if=/dev/zero of=" + FOTA_PATH};
@@ -69,29 +78,7 @@ public class FlashFota {
             //create a new root shell
             p = Runtime.getRuntime().exec("su");
             os = new DataOutputStream(p.getOutputStream());
-            //add the commands to the process
-            for (String tmpCmd : cmds) {
-                os.writeBytes(tmpCmd + "\n");
-            }
-            //flush out the process
-            os.writeBytes("exit\n");
-            os.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void backup() {
-        Log.d(LOG_TAG, "preparing to backup fota");
-        Process p;
-        DataOutputStream os;
-        //the backup command
-        String[] cmds = new String[]{
-                "dd if=" + FOTA_PATH + " of=" + dir.RAW_BACKUP_PATH};
-        try {
-            //create a new non root shell
-            p = Runtime.getRuntime().exec("su");
-            os = new DataOutputStream(p.getOutputStream());
+            is = p.getErrorStream();
             //add the commands to the process
             for (String tmpCmd : cmds) {
                 Log.d(LOG_TAG, tmpCmd);
@@ -99,17 +86,80 @@ public class FlashFota {
             }
             //flush out the process
             os.writeBytes("exit\n");
+            byte[] buff = new byte[512];
+            is.read(buff);
+            res += new String(buff);
             os.flush();
+            p.waitFor();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Toast succeeded = Toast.makeText
+                (c, c.getString(R.string.flashfota_format_succeeded),
+                        Toast.LENGTH_SHORT);
+        Toast failed = Toast.makeText
+                (c, c.getString(R.string.flashfota_format_failed),
+                        Toast.LENGTH_SHORT);
+
+        if (res.contains("No space left on device"))
+            succeeded.show();
+        else
+            failed.show();
+    }
+
+    public void backup() {
+        Log.d(LOG_TAG, "preparing to backup fota");
+        Process p;
+        DataOutputStream os;
+        InputStream is;
+        String res = "";
+        //the backup command
+        String[] cmds = new String[]{
+                "dd if=" + FOTA_PATH + " of=" + dir.RAW_BACKUP_PATH};
+        try {
+            //create a new non root shell
+            p = Runtime.getRuntime().exec("su");
+            os = new DataOutputStream(p.getOutputStream());
+            is = p.getErrorStream();
+            //add the commands to the process
+            for (String tmpCmd : cmds) {
+                Log.d(LOG_TAG, tmpCmd);
+                os.writeBytes(tmpCmd + "\n");
+            }
+            //flush out the process
+            os.writeBytes("exit\n");
+            byte[] buff = new byte[512];
+            is.read(buff);
+            res += new String(buff);
+            os.flush();
+            p.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        Toast succeeded = Toast.makeText
+                (c, c.getString(R.string.flashfota_backup_succeeded),
+                        Toast.LENGTH_SHORT);
+        Toast failed = Toast.makeText
+                (c, c.getString(R.string.flashfota_backup_failed),
+                        Toast.LENGTH_SHORT);
+
+        if (res.contains("bytes transferred in"))
+            succeeded.show();
+        else
+            failed.show();
     }
 
     public void restore() {
         Log.d(LOG_TAG, "preparing to restore fota");
         Process p;
         DataOutputStream os;
+        InputStream is;
+        String res = "";
         //the format command
         String[] cmds = new String[]{
                 "dd if=" + dir.RAW_BACKUP_PATH + " of=" + FOTA_PATH};
@@ -117,22 +167,43 @@ public class FlashFota {
             //create a new root shell
             p = Runtime.getRuntime().exec("su");
             os = new DataOutputStream(p.getOutputStream());
+            is = p.getErrorStream();
             //add the commands to the process
             for (String tmpCmd : cmds) {
                 os.writeBytes(tmpCmd + "\n");
             }
             //flush out the process
             os.writeBytes("exit\n");
+            os.writeBytes("exit\n");
+            byte[] buff = new byte[512];
+            is.read(buff);
+            res += new String(buff);
             os.flush();
+            p.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        Toast succeeded = Toast.makeText
+                (c, c.getString(R.string.flashfota_restore_succeeded),
+                        Toast.LENGTH_SHORT);
+        Toast failed = Toast.makeText
+                (c, c.getString(R.string.flashfota_restore_failed),
+                        Toast.LENGTH_SHORT);
+
+        if (res.contains("bytes transferred in"))
+            succeeded.show();
+        else
+            failed.show();
     }
 
     public void flashimg(String imgPath) {
         Log.d(LOG_TAG, "preparing to flash fota with" + imgPath);
         Process p;
         DataOutputStream os;
+        InputStream is;
+        String res = "";
         //the format command
         String[] cmds = new String[]{
                 "dd if=" + imgPath + " of=" + FOTA_PATH};
@@ -140,15 +211,36 @@ public class FlashFota {
             //create a new root shell
             p = Runtime.getRuntime().exec("su");
             os = new DataOutputStream(p.getOutputStream());
+            is = p.getErrorStream();
             //add the commands to the process
             for (String tmpCmd : cmds) {
                 os.writeBytes(tmpCmd + "\n");
             }
             //flush out the process
             os.writeBytes("exit\n");
+            os.writeBytes("exit\n");
+            byte[] buff = new byte[512];
+            is.read(buff);
+            res += new String(buff);
             os.flush();
+            p.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        Toast succeeded = Toast.makeText
+                (c, c.getString(R.string.fotaflash_flash_succeeded),
+                        Toast.LENGTH_SHORT);
+        Toast failed = Toast.makeText
+                (c, c.getString(R.string.fotaflash_flash_failed),
+                        Toast.LENGTH_SHORT);
+
+        if (res.contains("bytes transferred in"))
+            succeeded.show();
+        else
+            failed.show();
     }
+
 }
